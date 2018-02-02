5265b0971c7656e9f8d1e18401e8bc8ee4c29f5a
robertbutacu <robert.butacu@yahoo.com>
2018-02-02 23:32:45 +0200

    MessageDecoderSpec ScalaTest dependency

---
+++ src/test/scala/coding/huffman/MessageDecoderSpec.scala
@@ -0,0 +1,37 @@
+package coding.huffman
+
+import adt.HuffmanEncodingTree.{Branch, HuffmanEncodingTree, Leaf}
+import org.scalatest.FlatSpec
+
+class MessageDecoderSpec extends FlatSpec {
+  val huffmanTree: HuffmanEncodingTree[String] =
+    Branch(
+      Branch(
+        Leaf(Some("A")),
+        Leaf(Some("B"))
+      ),
+      Branch(
+        Leaf(Some("C")),
+        Leaf()
+      )
+    )
+
+  "An encoded message " should "be decoded when it is a valid encoding " in {
+    assert(MessageDecoder(huffmanTree).decode(List(
+      One, Zero,//C
+      One, Zero,//C
+      One, Zero,//C
+      Zero, One,//B
+      Zero, Zero,//A
+      Zero, One//B
+    )) === Some(List("C", "C", "C", "B", "A", "B")))
+  }
+
+  "An encoded message " should "not be decoded when its encoding is bad " in {
+    assert(MessageDecoder(huffmanTree).decode(List(
+      One, One,
+      One, Zero
+    )) === None)
+  }
+
+}
